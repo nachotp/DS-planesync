0449e68905437fa2eb1065b9094ec796252267f6
francozp <31630191+francozp@users.noreply.github.com>
2018-11-05 00:36:28 -0300

    fix range

--- Airplane/avion.py
+++ Airplane/avion.py
@@ -18,24 +18,29 @@ def aterrizar(avion):
     with grpc.insecure_channel(avion.torre + ':50051') as channel:
         stub = towerHostStub(channel)
         input("[Avión - " + avion.numero + "] Presione enter para aterrizar")
-        print("[Avión - " + avion.numero + "Ciudad Torre, "+ avion.numero +", establecidos en radial de pista hacia Ciudad")
+        print("[Avión - " + avion.numero + "] Ciudad Torre, "+ avion.numero +", establecidos en radial de pista hacia Ciudad")
         respuesta =stub.requestLanding(ArrivingPlane(code=avion.numero, srcAirport=avion.torre,ip= avion.ip))
         avion.rwy = respuesta.runway
         if(avion.rwy==-1):
-            print("Pista ocupada, mantener 5000 pies a la espera de autorización.")
+            print("[Avión - " + avion.numero + "] Pista ocupada, mantener 5000 pies a la espera de autorización.")
             while(avion.rwy==-1):
                     pass
-        print("Autorizados para aterrizar en pista " + str(avion.rwy))
-        return respuesta.runway
+        print("[Avión - " + avion.numero + "] Autorizados para aterrizar en pista " + str(avion.rwy))
 
 def despegar(avion):
     with grpc.insecure_channel(avion.torre + ':50051') as channel:
+        print("Pista: " + str(avion.rwy))
         stub = towerHostStub(channel)
         input("[Avión - " + avion.numero + "] Presione enter para despegar")
         dest = input("Ingrese destino:\n")
-        print("Pasando por el Gate...\nTodos los pasajeros a bordo y combustible cargado.\nPidiendo instrucciones para despegar...")
+        print("[Avión - " + avion.numero + "] Pasando por el Gate...\nTodos los pasajeros a bordo y combustible cargado.\nPidiendo instrucciones para despegar...")
         auth = stub.requestTakeoff(DepartingPlane(code=avion.numero, runway = avion.rwy))
-        print("Llegue aqui")
+        avion.rwy = auth.runway
+        if(avion.rwy==-1):
+            print("[Avión - " + avion.numero + "] Pista ocupada por "+ auth.preCode + ", mantener posición hasta nuevo aviso.")
+            while(avion.rwy==-1):
+                    pass
+        print("[Avión - " + avion.numero + "] Autorizados para despegar en pista " + str(avion.rwy))
 
 
 class Airplane(planeHostServicer):
@@ -52,11 +57,12 @@ numero = avion[1]
 peso = input("Peso Máximo de carga [kg]:\n")
 combustible = input("Capacidad del tanque de combustible [lt]:\n")
 torre_inicial = input("Torre de Control inicial:\n")
-ip = "192.168.10.3"
+ip = "192.168.10.2"
 plane = Avion(aerolinea, numero,peso,torre_inicial,0,ip)
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 add_planeHostServicer_to_server(Airplane(plane),server)
 server.add_insecure_port('0.0.0.0:50051')
 server.start()
-plane.rwy = aterrizar(plane)
+
+aterrizar(plane)
 despegar(plane)
